736adfdde2b787d0d2718b83bdca5da95d89c570
RobeSantoro <santoro.robe@gmail.com>
2022-10-25 13:40:05 +0200

    Added Helpers

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ export default function App() {
 
     return (
         <Canvas
-            dpr={ [ 1, 2 ] } // default
+            dpr={ [1, 2] } // default
             gl={ {
                 antialias: true, // default
                 toneMapping: THREE.ACESFilmicToneMapping, // default
@@ -18,7 +18,7 @@ export default function App() {
                 fov: 45,
                 near: 0.01,
                 far: 300,
-                position: [ 3, 2, 6 ]
+                position: [3, 2, 6]
             } }
         >
             <Experience />
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,66 +1,54 @@
+import { Html, OrbitControls, TransformControls, PivotControls } from '@react-three/drei'
 import { useRef } from 'react'
-import { extend, useFrame, useThree } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import CustomObject from './customObject';
-
-extend({ OrbitControls })
+import './Experience.css'
 
 export default function Experience() {
 
-    // const { camera, gl } = useThree()
-    // console.log(camera);
-    // console.log(gl);
-
-    const CameraControls = () => {
-        // Get a reference to the Three.js Camera, and the canvas html element.
-        // We need these to setup the OrbitControls component.
-        // https://threejs.org/docs/#examples/en/controls/OrbitControls
-        const { camera, gl: { domElement } } = useThree();
-        // Ref to the controls, so that we can update them on every frame using useFrame
-        const controls = useRef();
-        useFrame((state) => controls.current.update());
-        return <orbitControls ref={ controls } args={ [ camera, domElement ] } />;
-    };
-
-
-    const groupRef = useRef()
     const cubeRef = useRef()
-
-    useFrame((state, delta) => {
-        cubeRef.current.rotation.y += delta * 4
-        groupRef.current.rotation.y -= delta
-
-        const angle = state.clock.elapsedTime
-
-        state.camera.position.x = Math.sin(angle) * 8
-        state.camera.position.z = Math.cos(angle) * 8
-        state.camera.lookAt(0, 0, 0)
-    })
+    const sphereRef = useRef()
 
     return <>
 
-        {/* <OrbitControls args={ [ camera, gl ] } /> */ }
-        <CameraControls />
+        <OrbitControls dampingFactor={ 0.05 } makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <directionalLight position={ [1, 2, 3] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <group ref={ groupRef } >
-            <mesh ref={ cubeRef } position={ [ 3, 0, 0 ] } rotation-x={ 0.0 } scale={ 1 }>
-                <boxGeometry />
-                <meshStandardMaterial color={ 'mediumpurple' } wireframe={ false } />
-            </mesh>
-            <mesh position={ [ -3, 0, 0 ] } rotation-x={ 0.0 } scale={ 1 }>
-                <sphereGeometry args={ [ .5, 32, 32 ] } />
-                <meshStandardMaterial color={ 'orange' } wireframe={ false } />
-            </mesh>
-        </group>
+        <mesh ref={ cubeRef } position={ [2, 0, 0] } rotation-x={ 0.0 } scale={ 1 }>
+            <boxGeometry />
+            <meshStandardMaterial color={ 'mediumpurple' } wireframe={ false } />
+            <Html wrapperClass='label' center>Box</Html>
+        </mesh>
+        <TransformControls object={ cubeRef } mode='translate' />
 
-        <mesh position-y={ -1 } scale={ 20 } rotation={ [ -Math.PI * .5, 0, 0 ] }>
+        <PivotControls
+            anchor={ [0, 0, 0] /* Anchor Relative to bounding Box */ }
+            depthTest={ false }
+            lineWidth={ 4 }
+            axisColors={ ['#9381ff', '#ff4d6d', '#7ae582'] }
+            fixed={ true }
+            scale={ 100 }
+        >
+            <mesh ref={ sphereRef } position={ [-3, 0, 0] } rotation-x={ 0.0 } scale={ 2 }>
+                <sphereGeometry args={ [.5, 32, 32] } />
+                <meshStandardMaterial color={ 'orange' } wireframe={ false } />
+                <Html
+                    position={ [0, 0, .6] }
+                    wrapperClass='label'
+                    center
+                    distanceFactor={ 8 }
+                    occlude={ [sphereRef, cubeRef] }
+                >
+                    Sphere 🏀
+                </Html>
+            </mesh>
+        </PivotControls>
+
+        <mesh position-y={ -1 } scale={ 20 } rotation={ [-Math.PI * .5, 0, 0] }>
             <planeGeometry />
             <meshBasicMaterial color={ 'greenyellow' } />
         </mesh>
 
-        <CustomObject />
+
     </>
 }
